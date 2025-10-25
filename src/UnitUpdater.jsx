69bcd7e8fa31a9f527ce5ca9a105d7f1c77318d5
Goooble <bivinbheemaiah@gmail.com>
2025-10-25 20:13:54 +0530

    feat: store page works

--- src/UnitUpdater.jsx
+++ src/UnitUpdater.jsx
@@ -1,26 +1,35 @@
-function UnitUpdater({units, setUnits}){
-    return (
-        <>
-        <button
-              onClick={() => {
-                if (Number(units) === 1) return;
-                setUnits(Number(units) - 1);
-              }}
-              className="aspect-square w-8 shrink-0 rounded-full bg-stone-400 p-1 font-bold"
-            >
-              -
-            </button>
-        <input
-              value={units}
-              onChange={(e) => {
-                let num = Number(e.target.value)
-                num>0?setUnits(num):setUnits(1);
-              }}
-              className="w-1/1 rounded-md border border-stone-500 text-center [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
-              type="number"
-            />
-        </>
-    )
+function UnitUpdater({ units, setUnits }) {
+  return (
+    <>
+      <button
+        onClick={() => {
+          if (Number(units) === 1) return;
+          setUnits(Number(units) - 1);
+        }}
+        className="aspect-square w-8 shrink-0 rounded-full bg-stone-400 p-1 font-bold"
+      >
+        -
+      </button>
+      <input
+        value={units}
+        onFocus={(e)=>e.target.select()}
+        onChange={(e) => {
+          let num = Number(e.target.value);
+          num > 0 ? setUnits(num) : setUnits(1);
+        }}
+        className="w-1/1 rounded-md border border-stone-500 text-center [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
+        type="number"
+      />
+      <button
+        onClick={() => {
+          setUnits(Number(units) + 1);
+        }}
+        className="aspect-square w-8 shrink-0 rounded-full bg-stone-400 p-1 font-bold"
+      >
+        +
+      </button>
+    </>
+  );
 }
 
-export default UnitUpdater
+export default UnitUpdater;
